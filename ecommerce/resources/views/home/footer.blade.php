c651699a303ab2ead4191231e3fd9ab4ce01268d
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2025-05-23 23:41:51 +0600

    Integrate HTML Template in Laravel Project

---
+++ ecommerce/resources/views/home/footer.blade.php
@@ -0,0 +1,13 @@
+ <footer class=" footer_section">
+      <div class="container">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By
+          <a href="https://html.design/">Web Tech Knowledge</a>
+        </p>
+      </div>
+    </footer>
+    <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="{{asset('js/custom.js')}}"></script>
