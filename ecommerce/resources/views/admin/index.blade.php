760fd092a4e015ae2e21479e8e74f8c8e33b1ae4
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2025-05-24 11:47:44 +0600

    Integrate Admin Template in Laravel Project

--- ecommerce/resources/views/admin/index.blade.php
+++ ecommerce/resources/views/admin/index.blade.php
@@ -1,24 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Admin Dashboard</h1>
-     <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-        {{-- <x-dropdown-link :href="route('logout')"
-        onclick="event.preventDefault();
-        this.closest('form').submit();">
-        {{ __('Log Out') }}
-        </x-dropdown-link> --}}
-
-        <input type="submit" value="Logout">
-
-    </form>
-</body>
+<html>
+  <head> 
+    @include('admin.css')
+  </head>
+  <body>
+   <header class="header">
+    @include('admin.header')
+   </header>
+    
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+     @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+          @include('admin.body')
+          </div>
+        </div>
+      </div>
+        </section>
+        <footer class="footer">
+          <div class="footer__block block no-margin-bottom">
+            <div class="container-fluid text-center">
+              <!-- Please do not remove the backlink to us unless you support us at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
+               <p class="no-margin-bottom">2018 &copy; Your company. Download From <a target="_blank" href="https://templateshub.net">Templates Hub</a>.</p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('admincss/js/front.js')}}"></script>
+  </body>
 </html>
