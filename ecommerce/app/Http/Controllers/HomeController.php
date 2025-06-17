2d1482491e9a085502709f0b182455b4388add1f
Md. Shojib Talukder <adilhasanshojib@gmail.com>
2025-06-17 13:17:28 +0600

    Displaying Product Data in Homepage

--- ecommerce/app/Http/Controllers/HomeController.php
+++ ecommerce/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
@@ -11,7 +12,8 @@ class HomeController extends Controller
         return view('admin.index');
     }
 
-    function home(){
-        return view('home.index');
+    public function home(){
+        $product=Product::all();
+        return view('home.index',compact('product'));
     }
 }
